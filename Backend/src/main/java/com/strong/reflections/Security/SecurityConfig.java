142f6b31ea73d276d12328c96b82ec6dd88360cb
sayeedajmal <sayeedajmala06@gmail.com>
2025-08-28 22:57:24 +0530

    remoed the cors

--- Backend/src/main/java/com/strong/reflections/Security/SecurityConfig.java
+++ Backend/src/main/java/com/strong/reflections/Security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/auth/**", "/actuator/version").permitAll();
                     auth.anyRequest().authenticated();
